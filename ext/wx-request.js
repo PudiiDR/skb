3a9ac014a611bb99ad3f151c0917326ae16638c2
PudiiDR <xuelaoyu1969f@126.com>
2021-04-12 09:54:54 +0800

    chart

---
+++ ext/wx-request.js
@@ -0,0 +1,50 @@
+function wx_request(page, remoteUrl, token, callBack) {
+  wx.request({
+    url: remoteUrl,
+    // method: 'POST',
+    header: {
+      'content-type': 'application/json', // 默认值
+      'token': token
+    },
+    
+    data: {},
+    success: function (res) {
+      console.log(res);
+      var result = res.data;
+      switch (result.code) {
+        case '0':   
+          if (callBack) {
+            callBack(result.data);
+          }
+          break;
+        case '-1': 
+          wx.removeStorage({
+            key: "user_token",
+            success: function (res) {
+              console.log("退出登录");
+              wx.showToast({
+                title: '退出成功',
+                duration: 2000,
+                success: function () {
+                  wx.navigateTo({
+                    url: '../login/login'
+                  });
+                }
+              });
+            }
+          });
+          break;
+        default :
+          wx.showToast({
+            title: '请求数据失败',
+            image: "../../images/icon-no.png",
+            mask: true,
+            duration: 1000,
+          })
+          break;
+      };
+
+    }
+  });
+}
+module.exports = wx_request;
